7e8a6e8bcf6cb65a3fc5c10ef9108337be92ef32
SeanThinkPad <sbingley22@gmail.com>
2024-05-02 13:23:51 +0100

    added cone to drone

--- src/components/Drone.jsx
+++ src/components/Drone.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
 import { useRef } from "react"
 import Props from "./Props"
+import { Cone } from "@react-three/drei"
 
 const Drone = ({ index }) => {
   const ref = useRef()
+  const coneRef = useRef()
   const name = "enemy-drone-"+index
   let droneModel = "Drone"
   if (index % 3 == 1) droneModel += "001"
@@ -12,7 +14,14 @@ const Drone = ({ index }) => {
   //console.log(droneModel)
   return (
     <group ref={ref} name={name}>
-      <Props name={droneModel} />
+      <Props name={droneModel}/>
+      <Cone
+        ref={coneRef}
+        args={[.1, 0.3, 3]} // radius, height, segments
+        position={[0, .4, 0]}
+        rotation={[0, 0, Math.PI]}
+        material-color={"red"}
+      />
     </group>
   )
 }
